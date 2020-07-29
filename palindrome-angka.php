c2342b14ceacee119e2ecf2b76e8f6ccb4dfe7d1
setadarunala <129setadarunala@gmail.com>
2020-07-29 23:58:15 +0700

    finish

---
+++ palindrome-angka.php
@@ -0,0 +1,32 @@
+<?php
+function palindrome($str)
+{
+    $output = false;
+    if (strrev($str) == $str) {
+
+        $output = true;
+    }
+    return $output;
+
+}
+
+function palindrome_angka($angka)
+{
+    if ($angka >= 1 && $angka <= 8) {
+        return $angka + 1;
+    }if (palindrome($angka)) {
+        $angka++;
+
+    }
+    while (palindrome($angka) == false) {
+        $angka++;
+    }
+    return $angka;
+}
+
+// TEST CASES
+echo palindrome_angka(8) . "<br>"; // 9
+echo palindrome_angka(10) . "<br>"; // 11
+echo palindrome_angka(117) . "<br>"; // 121
+echo palindrome_angka(175) . "<br>"; // 181
+echo palindrome_angka(1000) . "<br>"; // 1001
